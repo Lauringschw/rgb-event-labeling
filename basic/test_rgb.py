1aa25340442b38d7174810bf20dfb04ca3e3dfbc
Laurin Schwenter <gschwenter.laurin@aol.com>
2026-04-06 19:51:12 +0200

    fixed cat

--- basic/test_rgb.py
+++ basic/test_rgb.py
@@ -5,20 +5,15 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 index = 1
+frame_num = 1
 base = Path("/home/lau/Documents/test_1")
 
 category = "rock"   # "rock" or "paper" or "scissor"
 prefix = {"rock": "r", "paper": "p", "scissor": "s"}[category]
 
-sample_dir = base / f"{category}/{prefix}_{index}"
-frame_pattern = "Basler_a2A1920-160ucPRO__*.raw"
-frame_paths = sorted(sample_dir.glob(frame_pattern))
-if not frame_paths:
-    raise FileNotFoundError(f"No frame files matched: {sample_dir / frame_pattern}")
-
-frame = np.fromfile(frame_paths[0], dtype=np.uint8)
+frame = np.fromfile(f'/home/lau/Documents/test_1/{category}/{prefix}_{index}/Basler_a2A1920-160ucPRO__40648144__20260402_143142134_{frame_num:04d}.raw', dtype=np.uint8)
 frame = frame.reshape(1200, 1920)
 
 plt.imshow(frame, cmap='gray')
-plt.title(f'Basler frame: {frame_paths[0].name}')
+plt.title(f'basler frame {frame_num:04d}')
 plt.show()
